91de0be3359b5ef983c274c972fd1ad5dfa68d13
Evanilson Pereira <pevanilson99@gmail.com>
2024-04-02 15:32:44 -0300

    Spring MVC Validation - Required Fields - PT4

--- src/main/java/com/luv2code/validationdemo/CustomerController.java
+++ src/main/java/com/luv2code/validationdemo/CustomerController.java
@@ -1,8 +1,12 @@
 package com.luv2code.validationdemo;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
@@ -10,5 +14,15 @@ public class CustomerController {
     public String showForm(Model theModel) {
         theModel.addAttribute("customer", new Customer());
         return "customer-form";
+    }
+
+    @PostMapping("/processForm")
+    public String processForm(@Valid @ModelAttribute("customer") Customer theCostumer, BindingResult theBindingResult) {
+        if(theBindingResult.hasErrors()) {
+            return "customer-form";
+        }
+        else {
+            return "customer-confirmation";
+        }
     }
 }
