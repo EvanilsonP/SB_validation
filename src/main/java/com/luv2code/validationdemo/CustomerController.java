b64605300c6840d75b6659c02db6950b320e0600
Evanilson Pereira <pevanilson99@gmail.com>
2024-04-02 16:47:38 -0300

    Spring MVC Validation - @InitBinder

--- src/main/java/com/luv2code/validationdemo/CustomerController.java
+++ src/main/java/com/luv2code/validationdemo/CustomerController.java
@@ -1,10 +1,13 @@
 package com.luv2code.validationdemo;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -18,11 +21,20 @@ public class CustomerController {
 
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCostumer, BindingResult theBindingResult) {
+
+        System.out.println("Last name: | " + theCostumer.getLastName() + "|");
         if(theBindingResult.hasErrors()) {
             return "customer-form";
         }
         else {
             return "customer-confirmation";
         }
+    }
+
+    // preventing white spaces
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 }
