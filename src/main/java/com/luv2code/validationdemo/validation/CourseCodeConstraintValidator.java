9c4257debe4d17a380aa900e6b8b4bf747e3ae4e
Evanilson Pereira <pevanilson99@gmail.com>
2024-04-03 09:57:10 -0300

    Spring MVC Validation - Custom Validation - PT5

--- src/main/java/com/luv2code/validationdemo/validation/CourseCodeConstraintValidator.java
+++ src/main/java/com/luv2code/validationdemo/validation/CourseCodeConstraintValidator.java
@@ -9,12 +9,38 @@ public class CourseCodeConstraintValidator implements ConstraintValidator<Course
 
     @Override
     public void initialize(CourseCode theCourseCode) {
-        coursePrefix = theCourseCode.value();
+        boolean result;
     }
 
     @Override
     public boolean isValid(String theCode, ConstraintValidatorContext theConstraintValidatorContext) {
-        boolean result = theCode.startsWith(coursePrefix);
+        boolean result;
+
+        if(theCode != null) {
+             result = theCode.startsWith(coursePrefix);
+        } else {
+            result = true;
+        }
+
         return false;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
